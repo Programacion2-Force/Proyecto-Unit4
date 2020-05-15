c1fddd5aabfa2ce85ab300ba2e487160b81dce8d
JCastillo721 <vladimircastillo14@yahoo.es>
2020-05-14 21:03:58 -0600

    ggg
    hhh

--- Restaurante_La_Lucha/Registro.aspx
+++ Restaurante_La_Lucha/Registro.aspx
@@ -3,10 +3,20 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <br /><br />
-    <asp:TextBox ID="TextBox1" runat="server" placeholder="Nombre"></asp:TextBox><br />
-    <asp:TextBox ID="TextBox2" runat="server" placeholder="Apellidos"></asp:TextBox><br />
-    <asp:TextBox ID="TextBox3" runat="server" placeholder="UserName"></asp:TextBox><br />
-    <asp:TextBox ID="TextBox4" runat="server" placeholder="Password"></asp:TextBox><br />
-    <asp:TextBox ID="TextBox5" runat="server" placeholder="Email"></asp:TextBox><br />
+    <asp:TextBox ID="TextBox1" runat="server" placeholder="Nombre"></asp:TextBox>
+    <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="TextBox1" ErrorMessage="Digite su nombre"></asp:RequiredFieldValidator>
+    <br />
+    <asp:TextBox ID="TextBox2" runat="server" placeholder="Apellidos"></asp:TextBox>
+    <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="TextBox2" ErrorMessage="Digite sus apellidos"></asp:RequiredFieldValidator>
+    <br />
+    <asp:TextBox ID="TextBox3" runat="server" placeholder="UserName"></asp:TextBox>
+    <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ControlToValidate="TextBox3" ErrorMessage="Cree nombre su de Usuario"></asp:RequiredFieldValidator>
+    <br />
+    <asp:TextBox ID="TextBox4" runat="server" placeholder="Password" TextMode="Password"></asp:TextBox>
+    <asp:RequiredFieldValidator ID="RequiredFieldValidator4" runat="server" ControlToValidate="TextBox4" ErrorMessage="Cree una contraseña"></asp:RequiredFieldValidator>
+    <br />
+    <asp:TextBox ID="TextBox5" runat="server" placeholder="Email" TextMode="Email"></asp:TextBox>
+    <asp:RequiredFieldValidator ID="RequiredFieldValidator5" runat="server" ControlToValidate="TextBox5" ErrorMessage="Proporcione su Email"></asp:RequiredFieldValidator>
+    <br />
     <asp:Button ID="Button1" runat="server" Text="Registrarme" OnClick="Button1_Click" />
 </asp:Content>
--- Restaurante_La_Lucha/Registro.aspx.designer.cs
+++ Restaurante_La_Lucha/Registro.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace Restaurante
         protected global::System.Web.UI.WebControls.TextBox TextBox1;
 
         /// <summary>
+        /// Control RequiredFieldValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+
+        /// <summary>
         /// Control TextBox2.
         /// </summary>
         /// <remarks>
@@ -31,6 +40,15 @@ namespace Restaurante
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox2;
+
+        /// <summary>
+        /// Control RequiredFieldValidator2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator2;
 
         /// <summary>
         /// Control TextBox3.
@@ -42,6 +60,15 @@ namespace Restaurante
         protected global::System.Web.UI.WebControls.TextBox TextBox3;
 
         /// <summary>
+        /// Control RequiredFieldValidator3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator3;
+
+        /// <summary>
         /// Control TextBox4.
         /// </summary>
         /// <remarks>
@@ -51,6 +78,15 @@ namespace Restaurante
         protected global::System.Web.UI.WebControls.TextBox TextBox4;
 
         /// <summary>
+        /// Control RequiredFieldValidator4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator4;
+
+        /// <summary>
         /// Control TextBox5.
         /// </summary>
         /// <remarks>
@@ -58,6 +94,15 @@ namespace Restaurante
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox5;
+
+        /// <summary>
+        /// Control RequiredFieldValidator5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator5;
 
         /// <summary>
         /// Control Button1.
